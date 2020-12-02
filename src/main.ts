ca2c27ea325abca16ff31b6781ecb7881e587e2f
susumi <8033078+susumi@user.noreply.gitee.com>
2020-12-02 21:24:43 +0800

    初始化配置less、axios封装、公共样式封装

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import 'lib-flexible/flexible';
+import {Button} from 'vant';
 
-Vue.config.productionTip = false
+
+Vue.use(Button);
+Vue.config.productionTip = false;
+(window as any).vRouter = router;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
