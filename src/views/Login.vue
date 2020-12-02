66b89bf4daa1a7f60a8725fc39e6b73eb8728d0a
susumi <8033078+susumi@user.noreply.gitee.com>
2020-12-03 05:01:51 +0800

    判断验证码输入成功失败

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -20,7 +20,9 @@
           :rules="[{ required: true, message: '请填写密码' }]"
         />
         <div class="verify">
-          <Verify :showButton="false" :width="'100%'" :height="'40px'" :fontSize="'16px'" :type="1"/>
+          <Verify ref="loginVerifyRef" :showButton="false" :width="'100%'" :height="'40px'" :fontSize="'16px'" :type="1"
+                  @success="successFun" @error="errorFun"
+          />
         </div>
         <div style="margin: 16px;">
           <van-button round block type="info" native-type="submit">
@@ -36,52 +38,82 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import ItemHeader from '@/components/ItemHeader.vue';
-  import Verify from '../../node_modules/vue2-verify/src/components/Verify.vue'
+  import Verify from '../../node_modules/vue2-verify/src/components/Verify.vue';
+  import {Toast} from 'vant';
 
   @Component({
-    components: {ItemHeader,Verify}
+    components: {ItemHeader, Verify}
   })
   export default class Login extends Vue {
     type = 'login';
     username = '';
     password = '';
+    verify = false;
 
     onSubmit(values: string) {
-      console.log('submit', values);
+      const verifySubmit = () => {
+        (this.$refs.loginVerifyRef as any).$refs.instance.checkCode();
+      };//可以提取到外面，但是我为了方便看就放在这里
+      verifySubmit();  //调用这个方法就会触发successFun/errorFun
+      if (!this.verify){
+        Toast.fail('验证码未填或填写错误') //这个方法可以哦——vant表单方法
+      }
+    }
+
+    successFun() {
+      // console.log('成功');
+      this.verify = true;
+    }
+
+    errorFun() {
+      // console.log('失败');
+      this.verify = false;
     }
   }
 </script>
 
 <style lang="less" scoped>
   @import "~@/common/style/mixin";
+
   .login {
     img {
       width: 100%;
     }
-    .login-message{
-      .login-from{
-        .verify{
-          ::v-deep:nth-child(1){
+
+    .login-message {
+      padding: 16px;
+
+      .login-from {
+        .verify {
+          margin-top: 16px;
+
+          ::v-deep:nth-child(1) {
             .fjcc();
             height: 40px;
-            .verify-code{
+
+            .verify-code {
               margin: 0;
             }
-            .cerify-code-panel{
+
+            .cerify-code-panel {
               .fjbt();
               height: 40px;
             }
-            .verify-change-area{
+
+            .verify-change-area {
               font-size: 12px;
             }
-            .verify-code-area{
+
+            .verify-code-area {
               height: 100%;
             }
-            .verify-input-area{
+
+            .verify-input-area {
               padding-left: 6px;
               height: 100%;
             }
-            .varify-input-code{
+
+            .varify-input-code {
               height: 90%;
               border: 1px solid #ddd;
               padding: 0 6px;
