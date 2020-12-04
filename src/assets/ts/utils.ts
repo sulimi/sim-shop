45c5852c2ae1582c6a18dd2cf8d58a3eea8edd9d
susumi <8033078+susumi@user.noreply.gitee.com>
2020-12-04 20:01:20 +0800

    图片路径加工方法提取

--- src/assets/ts/utils.ts
+++ src/assets/ts/utils.ts
@@ -5,3 +5,14 @@ export const getLocal = (name: string) => {
 export const setLocal = (name: string, value: string) => {
   localStorage.setItem(name, value);
 };
+
+
+// 图片前缀方法
+export const prefix = (url: string) => {
+  if (url && url.startsWith('http')) {
+    return url
+  } else {
+    url = `http://47.99.134.126:28019${url}`
+    return url
+  }
+}
