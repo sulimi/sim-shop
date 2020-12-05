72e2b810d4852ee0e860cabfc5fab31f72e94d70
susumi <8033078+susumi@user.noreply.gitee.com>
2020-12-05 17:26:07 +0800

    进步器按钮点击触发两次修改函数解决方案

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -15,7 +15,7 @@
             </div>
             <div class="good-btn">
               <div class="money">¥{{ item.sellingPrice }}</div>
-             <!--商品+1-1按钮 vant进步器-->
+              <!--商品+1-1按钮 vant进步器-->
               <van-stepper
                 integer
                 :min="1"
@@ -48,7 +48,7 @@
   })
   export default class Cart extends Vue {
     list = []; // 购物车商品列表
-    result = [];// 购物车商品的 id 数组
+    result = [];// 购物车商品的 id 数组，用于多选
     mounted() {
       this.init();
     }
@@ -64,17 +64,20 @@
     }
 
     //进步器的异步触发方法
-    onChange(value: any, clickItem: {name: number}) {
+    onChange(value: any, clickItem: { name: number }) {
       //value是按钮显示的值，clickItem是整个按钮的信息
-      Toast.loading({ forbidClick: true });
+      // 注意此时修改 value 后会再次触发 change 事件
+      //解决二次触发：
+      if ((this.list.filter(i => (i as any).cartItemId === clickItem.name)[0] as any).goodsCount === value) return;
+
+      Toast.loading({forbidClick: true});
       setTimeout(() => {
         Toast.clear();
-        // 注意此时修改 value 后会再次触发 change 事件
-        this.list.forEach(item => {
-          if(clickItem.name=== (item as any).cartItemId){
-            (item as any).goodsCount=value
+        this.list.forEach(i => {
+          if (clickItem.name === (i as any).cartItemId) {
+            (i as any).goodsCount = value;
           }
-        })
+        });
 
       }, 500);
     }
@@ -83,49 +86,58 @@
 
 <style lang="less" scoped>
   @import "~@/assets/style/mixin.less";
-.cart-wrapper{
-  .cart-content {
-    margin: 50px 0 100px 0;
-    padding-left: 10px;
-    .good-item {
-      display: flex;
-      .good-img {
-        img {
-          .wh(100px, 100px)
-        }
-      }
-      .good-text {
+
+  .cart-wrapper {
+    .cart-content {
+      margin: 50px 0 100px 0;
+      padding-left: 10px;
+
+      .good-item {
         display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        /*flex-grow: 1;*/
-        /*flex-shrink: 1;*/
-        /*flex-basis: 0;*/
-        flex: 1;
-        padding: 20px;
-        .good-title {
-          display: flex;
-          justify-content: space-between;
-        }
-        .good-btn {
-          display: flex;
-          justify-content: space-between;
-          .money {
-            font-size: 16px;
-            color: red;
-            line-height: 28px;
+
+        .good-img {
+          img {
+            .wh(100px, 100px)
           }
-          .van-icon-delete {
-            font-size: 20px;
-            margin-top: 4px;
+        }
+
+        .good-text {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          /*flex-grow: 1;*/
+          /*flex-shrink: 1;*/
+          /*flex-basis: 0;*/
+          flex: 1;
+          padding: 20px;
+
+          .good-title {
+            display: flex;
+            justify-content: space-between;
+          }
+
+          .good-btn {
+            display: flex;
+            justify-content: space-between;
+
+            .money {
+              font-size: 16px;
+              color: red;
+              line-height: 28px;
+            }
+
+            .van-icon-delete {
+              font-size: 20px;
+              margin-top: 4px;
+            }
           }
         }
       }
-    }
-    .delete-button {
-      width: 50px;
-      height: 100%;
+
+      .delete-button {
+        width: 50px;
+        height: 100%;
+      }
     }
   }
-}
 </style>
