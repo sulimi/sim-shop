d2f69c8838b3ec82135386a93eb3d140ad836c5d
susumi <8033078+susumi@user.noreply.gitee.com>
2020-12-07 22:08:20 +0800

    首屏其他页不需要返回按钮

--- src/components/ItemHeader.vue
+++ src/components/ItemHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="item-header header">
     <div class="btn-icon" @click="backFun">
-      <Icon name="left"/>
+      <Icon :name="leftIcon"/>
     </div>
     <div class="item-header-title" v-if="title">{{title}}</div>
     <slot v-else/>
     <div class="btn-icon" @click="onClickIcon">
-      <Icon :name="iconRight" />
+      <Icon :name="iconRight"/>
     </div>
   </header>
 </template>
@@ -24,13 +24,20 @@
     @Prop() title!: string;
     @Prop() iconRight!: string;
     @Prop() routerName!: string;
+
+    get leftIcon() {
+      return this.title === '购物车' || this.title === '我的' ? '' : 'left';
+    }
+
     onClickIcon() {
-      Toast.setDefaultOptions({ duration: 500 });
+      Toast.setDefaultOptions({duration: 500});
       Toast('未开发');
     }
+
     backFun() {
-      if (!this.routerName){
-        this.$router.go(-1)
+      if (!this.leftIcon) return;
+      if (!this.routerName) {
+        this.$router.go(-1);
       }
       this.$router.push({path: this.routerName});
     }
