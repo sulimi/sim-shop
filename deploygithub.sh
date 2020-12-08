1f5b46bc72ba8dc14197815858e257754bef5d96
susumi <8033078+susumi@user.noreply.gitee.com>
2020-12-08 08:32:34 +0800

    部署

---
+++ deploygithub.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git remote add origin git@github.com:sulimi/sim-shop.git&&
+git branch gh-pages &&
+git push -u origin gh-pages -f &&
+cd -
