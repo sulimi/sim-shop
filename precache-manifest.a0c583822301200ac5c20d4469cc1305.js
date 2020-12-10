cccfe323ae2d69f3397dd1cba45e440148eb6809
susumi <8033078+susumi@user.noreply.gitee.com>
2020-12-10 21:07:25 +0800

    deploy

---
+++ precache-manifest.a0c583822301200ac5c20d4469cc1305.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfa8467ca471722f4ce",
+    "url": "/sim-shop/css/app.cc118b28.css"
+  },
+  {
+    "revision": "0170a6db0bb3127fcbe9",
+    "url": "/sim-shop/css/chunk-vendors.78af29a4.css"
+  },
+  {
+    "revision": "3431bad4348d5ea8c7190ba4f034c4c8",
+    "url": "/sim-shop/img/head.3431bad4.jpg"
+  },
+  {
+    "revision": "698fa2a6d73bdc477421487c5a9e0e8c",
+    "url": "/sim-shop/img/login.698fa2a6.jpg"
+  },
+  {
+    "revision": "1b5394dfeffd1c4f6ce2c73e854c9062",
+    "url": "/sim-shop/index.html"
+  },
+  {
+    "revision": "7cfa8467ca471722f4ce",
+    "url": "/sim-shop/js/app.15ca5ffa.js"
+  },
+  {
+    "revision": "0170a6db0bb3127fcbe9",
+    "url": "/sim-shop/js/chunk-vendors.33b66567.js"
+  },
+  {
+    "revision": "7f220db8ce2e264526b218f5d9700a10",
+    "url": "/sim-shop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sim-shop/robots.txt"
+  }
+]);
